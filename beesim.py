3dd84788aeab43da2775cbda2d437ac0b5b9bc60
nekechs <nekechs.dev@gmail.com>
2023-06-19 15:57:03 -0400

    make_vid_csv.py: Creates 150 videos of 1024 frames each, along with a dataset.csv

--- beesim.py
+++ beesim.py
@@ -4,10 +4,19 @@ import random
 import math
 import time
 import bee
+import os
+import subprocess
+
+# setup for video extraction
+frames_dir = "frames"
+if not os.path.exists(frames_dir):
+    os.makedirs(frames_dir)
+for f in os.listdir(frames_dir):
+    os.remove(os.path.join(frames_dir, f))
 
 # In this function, there is a 3 percent chance that we will turn CW by 0.1 radians.
 def update_custom(instance):
-    prob = 0.03
+    prob = 0.3
     if random.random() < prob:
         instance.rot += 0.1
 
@@ -29,12 +38,11 @@ bees = []
 for i in range(1):
     bees.append(bee.Bee(screen, 2, 0.15, custom_update=update_custom))
 
-# Run the simulation for 2 minutes (120 seconds)
-while time.time() - start_time < 120:
+frame_number=0
+while frame_number < 10000:
     #selecting the framerate
     clock.tick(60)
 
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -49,12 +57,19 @@ while time.time() - start_time < 120:
     
     fps = int(clock.get_fps())
     fps_text = font.render(f"FPS: {fps}", True, (0, 0, 0))
-    screen.blit(fps_text, (10, 10))
-
+    # screen.blit(fps_text, (10, 10))
 
     # Update the display
     pygame.display.flip()
-    
 
-# End the program after 2 minutes
+    # Save the current frame as an image on disk
+    frame_filename = os.path.join(frames_dir, f"frame_{frame_number:06d}.png")
+    pygame.image.save(screen, frame_filename)
+
+    frame_number += 1
+
+# End the program
 pygame.quit()
+
+output_filename = "output.h264"
+subprocess.call(["ffmpeg", "-r", "30", "-i", f"{frames_dir}/frame_%06d.png", "-c:v", "libx264", "-vf", "fps=60", output_filename])
---
+++ make_vid_csv.py
@@ -0,0 +1,84 @@
+import pygame
+import numpy as np
+import random
+import time
+from bee import Bee
+import os
+import subprocess
+import csv
+
+videos_dir="videos/first-set"
+
+# Some probability of turning right by 0.1 radians
+def update_custom(instance):
+    prob = 0.03
+    if random.random() < prob:
+        instance.rot += 0.1
+
+def generate_video(outfile, num_bees, fwd_amount, turn_amount, is_fullrand, screen_dim=(512, 512), video_dir=videos_dir):
+    # setup frames directory
+    frames_dir="frames"
+    if not os.path.exists(frames_dir):
+        os.makedirs(frames_dir)
+    if not os.path.exists(video_dir):
+        os.makedirs(video_dir)
+    for f in os.listdir(frames_dir):
+        os.remove(os.path.join(frames_dir, f))
+    
+    # Initialize some pygame stuff
+    (x_width, y_width) = screen_dim
+    pygame.init()
+    font = pygame.font.SysFont(None, 30)
+    screen = pygame.display.set_mode([x_width, y_width])
+    start_time = time.time()
+    clock = pygame.time.Clock()
+
+    bees=[]
+    for i in range(num_bees):
+        bees.append(Bee(screen, fwd_amount, turn_amount, x_width=x_width, y_width=y_width))
+    
+    frame_number = 0
+    while frame_number < 1024:  # Each of these is a frame
+        # clock.tick(120)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        start_frame_time = time.time()
+        screen.fill((255, 255, 255))
+        for bee in bees:
+            bee.update()
+            bee.draw()
+        
+        fps = int(clock.get_fps())
+        fps_text = font.render(f"FPS: {fps}", True, (0, 0, 0))
+
+        # Display stuff - Save images
+        pygame.display.flip()
+        frame_filename = os.path.join(frames_dir, f"frame_{frame_number:06d}.png")
+        pygame.image.save(screen, frame_filename)
+
+        frame_number += 1
+    
+    pygame.quit()
+    output_file = f"{os.path.join(video_dir, outfile)}.h264"
+    subprocess.call(["ffmpeg", "-r", "30", "-i", f"{frames_dir}/frame_%06d.png", "-c:v", "libx264", "-vf", "fps=60", output_file])
+    return output_file
+
+for f in os.listdir(videos_dir):
+    os.remove(os.path.join(videos_dir, f))
+
+with open("students.csv", 'w', newline='') as file:
+    writer = csv.writer(file)
+    field = ["file", "class", "begin frame", "end frame"]
+    writer.writerow(field)
+    num_videos = 150
+    for video_number in range(num_videos):
+        if random.random() < 0.5:
+            file = generate_video(f"sample{video_number}", 1, 2, 0.15, True)
+            nn_class=1
+        else:
+            file = generate_video(f"sample{video_number}", 1, 2, 0.15, False)
+            nn_class=2
+        writer.writerow([file, nn_class, 50, 1023])
