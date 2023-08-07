cf818bce9c1b708e93d73973fba3af0bce43e776
nekechs <nekechs.dev@gmail.com>
2023-08-07 10:27:32 -0400

    Updated beesim.py to reflect the new model suggested by Dr. Martin

--- bee.py
+++ bee.py
@@ -3,6 +3,93 @@ import pygame
 import math
 import random
 
+POS_XAXIS = pygame.math.Vector2(1, 0)
+
+class PathBee:
+    def __init__(self, screen: pygame.surface.Surface, walk_strength, dot_radius = 20):
+        # Initialize the screen parameters
+        self.screen = screen
+        (self.x_width, self.y_width) = screen.get_size()
+
+        # Initialize position and rotation
+        # self.start_pos = pygame.math.Vector2(np.random.uniform(0, self.x_width),
+        #                                     np.random.uniform(0, self.y_width))
+        # self.end_pos = pygame.math.Vector2(np.random.uniform(0, self.x_width),
+        #                                     np.random.uniform(0, self.y_width))
+        self.start_pos = pygame.math.Vector2(360, 200)
+        self.end_pos = pygame.math.Vector2(200, 360)
+        self.current_pos = pygame.math.Vector2(self.start_pos)
+        self.rot = 2 * math.pi * random.random()
+        # self.reset()
+
+        goal_displacement = self.end_pos - self.start_pos
+        self.Dmax = goal_displacement.length()
+
+        self.dot_radius = dot_radius
+        self.dot_color = (255, 0, 0)
+
+        self.walk_strength = walk_strength
+
+        # Stuff stored to make the arrow
+        self.arrow_color = (255, 255, 0)
+        self.arrow_points = [
+            (-10, 0),
+            (10, 0),
+            (0, -20),
+            (-10, 0)
+        ]
+
+    def reset(self):
+        self.start_pos = pygame.math.Vector2(np.random.uniform(0, self.x_width),
+                                            np.random.uniform(0, self.y_width))
+        self.end_pos = pygame.math.Vector2(np.random.uniform(0, self.x_width),
+                                            np.random.uniform(0, self.y_width))
+        self.current_pos = pygame.math.Vector2(self.start_pos)
+        self.rot = 2 * math.pi * random.random()
+
+    # F is the amount of field distortion.
+    def update(self, F):
+        goal_displacement = (self.end_pos - self.current_pos)
+        dist = goal_displacement.length()
+        if dist < self.dot_radius:
+            return
+        direct_angle = POS_XAXIS.angle_to(goal_displacement)
+
+        field_distortion = F * (1 - math.exp(- direct_angle**2))
+        if direct_angle >= 0:
+            J = min(360, direct_angle + field_distortion)
+        else:
+            direct_angle += 360
+            J = max(0, direct_angle - field_distortion)
+
+        # if direct_angle < 0:
+        #     direct_angle += 360
+        # if self.rot < 180:
+        #     J = min(360, self.rot + F)
+        # else:
+        #     J = max(0, self.rot - F)
+
+        print(f"Direct angle: {direct_angle}; J: {J}")
+
+        W1 = (dist - self.Dmax) ** 2 / (self.Dmax ** 2)
+        W2 = 1 - W1
+        self.rot = W1 * direct_angle + W2 * J
+        print(f"{self.rot}\n")
+        self.current_pos += POS_XAXIS.rotate(self.rot) * self.walk_strength
+
+    def draw(self):
+        # Draw the circle
+        pygame.draw.circle(self.screen, self.dot_color, self.current_pos, self.dot_radius)
+        pygame.draw.circle(self.screen, pygame.Color("blue"), self.start_pos, self.dot_radius)
+        pygame.draw.circle(self.screen, pygame.Color("green"), self.end_pos, self.dot_radius)
+
+        # Draw the triangle
+        rotated_arrow_points = []
+        for point in self.arrow_points:
+            rotated_point = pygame.math.Vector2(point).rotate(self.rot + 90)
+            rotated_arrow_points.append((rotated_point.x + self.current_pos.x, rotated_point.y + self.current_pos.y))
+        pygame.draw.polygon(self.screen, self.arrow_color, rotated_arrow_points)
+
 class Bee:
     def __init__(self, screen: pygame.surface.Surface, walk_strength, custom_update=None, dot_radius=20):
         # Initialize the screen parameters
--- beesim.py
+++ beesim.py
@@ -1,61 +1,88 @@
 import pygame
 import time
-from bee import Bee
+from bee import Bee, PathBee
 import os
 import subprocess
 from datetime import datetime
 import random
 
-# Combines all images from the "frames" directory into one video using ffmpeg
-def assemble_video(filename, frames_dir="frames", video_dir="videos"):
-    if not os.path.exists(video_dir):
-        os.makedirs(video_dir)
-    for f in os.listdir(video_dir):
-        os.remove(os.path.join(video_dir, f))
-
-    outfile_path = os.path.join(video_dir, f"{filename}.h264")
-    subprocess.call(["ffmpeg", "-r", "30", "-i", f"{frames_dir}/frame_%06d.png", "-c:v", "libx264", "-vf", "fps=60", outfile_path])
-    return outfile_path
-
-def make_frames(outfile, classnum, screen_dim, num_bees=1, fwd_amount=2, frames_dir="frames", video_dir="videos"):
-        # Initialize some pygame stuff
-    (x_dim, y_dim) = screen_dim
+def test_pathbee():
+    (x_dim, y_dim) = (512, 512)
     pygame.init()
-    font = pygame.font.SysFont(None, 30)
     screen = pygame.display.set_mode([x_dim, y_dim])
     start_time = time.time()
     clock = pygame.time.Clock()
 
-    if not os.path.exists(frames_dir):
-        os.makedirs(frames_dir)
-    for f in os.listdir(frames_dir):
-        os.remove(os.path.join(frames_dir, f))
-    
-    bees=[]
-    for i in range(num_bees):
-        bees.append(Bee(screen, fwd_amount, x_width=x_dim, y_width=y_dim))
-    
-    frame_number = 0
-    while frame_number < 1024:  # Each of these is a frame
-        # clock.tick(120)
+    # bee = PathBee(screen, 5.0, 20)
+    bees = []
+    for _ in range(9):
+        bees.append(PathBee(screen, 10, 20))
+    for frame_number in range(1024):
+        clock.tick(10)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
 
-        start_frame_time = time.time()
         screen.fill((255, 255, 255))
-        for bee in bees:
-            bee.update()
+        for (i, bee) in enumerate(bees):
+            bee.update(i * 10)
             bee.draw()
-        
-        fps = int(clock.get_fps())
-        fps_text = font.render(f"FPS: {fps}", True, (0, 0, 0))
 
-        # Display stuff - Save images
         pygame.display.flip()
-        frame_filename = os.path.join(frames_dir, f"frame_{frame_number:06d}.png")
-        pygame.image.save(screen, frame_filename)
-        frame_number += 1
+
+test_pathbee()
+
+# Combines all images from the "frames" directory into one video using ffmpeg
+# def assemble_video(filename, frames_dir="frames", video_dir="videos"):
+#     if not os.path.exists(video_dir):
+#         os.makedirs(video_dir)
+#     for f in os.listdir(video_dir):
+#         os.remove(os.path.join(video_dir, f))
+
+#     outfile_path = os.path.join(video_dir, f"{filename}.h264")
+#     subprocess.call(["ffmpeg", "-r", "30", "-i", f"{frames_dir}/frame_%06d.png", "-c:v", "libx264", "-vf", "fps=60", outfile_path])
+#     return outfile_path
+
+# def make_frames(outfile, classnum, screen_dim, num_bees=1, fwd_amount=2, frames_dir="frames", video_dir="videos"):
+#         # Initialize some pygame stuff
+#     (x_dim, y_dim) = screen_dim
+#     pygame.init()
+#     font = pygame.font.SysFont(None, 30)
+#     screen = pygame.display.set_mode([x_dim, y_dim])
+#     start_time = time.time()
+#     clock = pygame.time.Clock()
+
+#     if not os.path.exists(frames_dir):
+#         os.makedirs(frames_dir)
+#     for f in os.listdir(frames_dir):
+#         os.remove(os.path.join(frames_dir, f))
     
-    pygame.quit()
+#     bees=[]
+#     for i in range(num_bees):
+#         bees.append(Bee(screen, fwd_amount, x_width=x_dim, y_width=y_dim))
+    
+#     frame_number = 0
+#     while frame_number < 1024:  # Each of these is a frame
+#         # clock.tick(120)
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 pygame.quit()
+#                 exit()
+
+#         start_frame_time = time.time()
+#         screen.fill((255, 255, 255))
+#         for bee in bees:
+#             bee.update()
+#             bee.draw()
+        
+#         fps = int(clock.get_fps())
+#         fps_text = font.render(f"FPS: {fps}", True, (0, 0, 0))
+
+#         # Display stuff - Save images
+#         pygame.display.flip()
+#         frame_filename = os.path.join(frames_dir, f"frame_{frame_number:06d}.png")
+#         pygame.image.save(screen, frame_filename)
+#         frame_number += 1
+    
+#     pygame.quit()
